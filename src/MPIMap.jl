b6c2d1352e2936c8d75a530b426f850bbc11e463
Gu Junhua <jhgu@nao.cas.cn>
2020-08-18 10:29:57 +0800

    modified:   src/MPIMap.jl

--- src/MPIMap.jl
+++ src/MPIMap.jl
@@ -13,7 +13,7 @@ module MPIMap
             reply_cnt=0
             
             result=Array{Union{Missing, RT}}(missing, size(data)...)
-            #result[1,1]=1.0
+            result[1,1]=1.0
             missing_idx=findall(reshape(map(ismissing,result), length(result)))
             n_tasks=length(missing_idx)
             println("waiting...")
