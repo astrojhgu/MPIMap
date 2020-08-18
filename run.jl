d28a6daad01a1edfb13f4085597097e1a48f5c2b
Gu Junhua <jhgu@nao.cas.cn>
2020-08-18 11:22:39 +0800

    modified:   run.jl

--- run.jl
+++ run.jl
@@ -9,7 +9,7 @@ comm = MPI.COMM_WORLD
 my_rank=MPI.Comm_rank(comm)
 n_procs=MPI.Comm_size(comm)
 
-a=zeros(15,15)
+input=zeros(15,15)
 function cb(result)
     s=sum(map(result) do x
         if ismissing(x)
@@ -22,13 +22,19 @@ function cb(result)
 end
 
 temp_result=Matrix{Union{Missing, Float64}}(missing, 15, 15)
-temp_result[1,1]=50.0
+# We force the [1,1] element to be 50, which is obviously a wrong result.
+# The purpose is to check if the mpi_map will touch this element
+# given that it has a value.
+temp_result[1,1]=50.0 
 
-result=mpi_map(a, comm, temp_result; mgr_cb=cb) do x
+result=mpi_map(input, comm, temp_result; mgr_cb=cb) do x
     x+1.0
 end
 
+answer=ones(size(input))
+answer[1,1]=50.0
 
 if my_rank==0
+    @assert answer==result
     println(result)
 end
